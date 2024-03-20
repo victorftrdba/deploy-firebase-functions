31bc84b458c4cea8f04fdfa9cae095428eb5c844
Victor Nogueira <69355100+victorftrdba@users.noreply.github.com>
2024-03-20 10:04:20 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,43 +1,85 @@
 #!/bin/bash
 
-cd /github/workspace/backend; npm install;
-touch /github/workspace/backend/.env;
-echo CLIENT_ID="${CLIENT_ID}" >> /github/workspace/backend/.env;
-echo CLIENT_SECRET="${CLIENT_SECRET}" >> /github/workspace/backend/.env;
-echo REDIRECT_URL="${REDIRECT_URL}" >> /github/workspace/backend/.env;
-echo REDIRECT_URL_2="${REDIRECT_URL_2}" >> /github/workspace/backend/.env;
-echo ENDPOINT="${ENDPOINT}" >> /github/workspace/backend/.env;
-echo TOPIC_NAME="${TOPIC_NAME}" >> /github/workspace/backend/.env;
-echo SUBSCRIPTION_NAME="${SUBSCRIPTION_NAME}" >> /github/workspace/backend/.env;
-echo MEILISEARCH_URL="${MEILISEARCH_URL}" >> /github/workspace/backend/.env;
-echo MEILISEARCH_KEY="${MEILISEARCH_KEY}" >> /github/workspace/backend/.env;
-echo MEILISEARCH_API_KEY="${MEILISEARCH_API_KEY}" >> /github/workspace/backend/.env;
-echo MEILISEARCH_UUID="${MEILISEARCH_UUID}" >> /github/workspace/backend/.env;
-echo REDIRECT_URL_BROWSER="${REDIRECT_URL_BROWSER}" >> /github/workspace/backend/.env;
-echo REDIRECT_URL_BROWSER_2="${REDIRECT_URL_BROWSER_2}" >> /github/workspace/backend/.env;
-echo TASKMIDDLEWARE_URL="${TASKMIDDLEWARE_URL}" >> /github/workspace/backend/.env;
-echo FIRESTORE_CERT="${FIRESTORE_CERT}" >> /github/workspace/backend/.env;
-echo ENVIRONMENT="${ENVIRONMENT}" >> /github/workspace/backend/.env;
-cat /github/workspace/backend/.env;
-
+# Verifica se FIREBASE_TOKEN está definido
 if [ -z "${FIREBASE_TOKEN}" ]; then
     echo "FIREBASE_TOKEN is missing"
     exit 1
 fi
 
+# Verifica se FIREBASE_PROJECT está definido
 if [ -z "${FIREBASE_PROJECT}" ]; then
     echo "FIREBASE_PROJECT is missing"
     exit 1
 fi
 
-firebase deploy \
-    -m "${GITHUB_REF} (${GITHUB_SHA})" \
-    --project ${FIREBASE_PROJECT} \
-    --only "functions:groupA" \
-    --force
+# Lista das pastas desejadas
+folders=(
+    "/github/workspace/applications"
+    "/github/workspace/auth"
+    "/github/workspace/backend"
+    "/github/workspace/cancel-application"
+    "/github/workspace/classroom-roster-change"
+    "/github/workspace/competency-matrices"
+    "/github/workspace/create-application"
+    "/github/workspace/jobs"
+    "/github/workspace/pubsub"
+    "/github/workspace/sync-meilisearch"
+    "/github/workspace/tasks"
+    "/github/workspace/users"
+)
 
-firebase deploy \
-    -m "${GITHUB_REF} (${GITHUB_SHA})" \
-    --project ${FIREBASE_PROJECT} \
-    --only "functions:groupB" \
-    --force
+# Loop sobre cada pasta
+for folder in "${folders[@]}"
+do
+    # Navega para a pasta
+    cd "$folder";
+    
+    # Instalação do npm
+    npm install;
+    
+    # Criação do arquivo .env e atribuição de variáveis
+    touch "${folder}/.env";
+    
+    # Escrita das variáveis no .env
+    echo CLIENT_ID="${CLIENT_ID}" >> "${folder}/.env";
+    echo CLIENT_SECRET="${CLIENT_SECRET}" >> "${folder}/.env";
+    echo REDIRECT_URL="${REDIRECT_URL}" >> "${folder}/.env";
+    echo REDIRECT_URL_2="${REDIRECT_URL_2}" >> "${folder}/.env";
+    echo ENDPOINT="${ENDPOINT}" >> "${folder}/.env";
+    echo TOPIC_NAME="${TOPIC_NAME}" >> "${folder}/.env";
+    echo SUBSCRIPTION_NAME="${SUBSCRIPTION_NAME}" >> "${folder}/.env";
+    echo MEILISEARCH_URL="${MEILISEARCH_URL}" >> "${folder}/.env";
+    echo MEILISEARCH_KEY="${MEILISEARCH_KEY}" >> "${folder}/.env";
+    echo MEILISEARCH_API_KEY="${MEILISEARCH_API_KEY}" >> "${folder}/.env";
+    echo MEILISEARCH_UUID="${MEILISEARCH_UUID}" >> "${folder}/.env";
+    echo REDIRECT_URL_BROWSER="${REDIRECT_URL_BROWSER}" >> "${folder}/.env";
+    echo REDIRECT_URL_BROWSER_2="${REDIRECT_URL_BROWSER_2}" >> "${folder}/.env";
+    echo TASKMIDDLEWARE_URL="${TASKMIDDLEWARE_URL}" >> "${folder}/.env";
+    echo FIRESTORE_CERT="${FIRESTORE_CERT}" >> "${folder}/.env";
+    echo ENVIRONMENT="${ENVIRONMENT}" >> "${folder}/.env";
+
+    # Exibe o conteúdo do .env
+    cat "${folder}/.env";
+
+    # Lista dos arquivos de grupo
+    groupFiles=(
+        "group-a.ts"
+        "group-b.ts"
+        "group-c.ts"
+        "group-d.ts"
+    )
+
+    # Loop sobre cada arquivo para verificar se existe
+    # e iniciar processo de implantação
+    for groupFile in "${groupFiles[@]}"
+    do
+        # Verifica se o arquivo existe
+        if [ -f "${groupFile}" ]; then
+            firebase deploy \
+                -m "${GITHUB_REF} (${GITHUB_SHA})" \
+                --project "${FIREBASE_PROJECT}" \
+                --only "functions:${groupFile}.ts" \
+                --force
+        fi
+    done
+done
